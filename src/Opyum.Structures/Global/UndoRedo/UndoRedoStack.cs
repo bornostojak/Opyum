659185a48d1a4d0f9e406ca549ea596d62da96b8
--unset <22800507+bornostojak@users.noreply.github.com>
2021-04-27 13:12:28 +0200

    Rename source to src

---
+++ src/Opyum.Structures/Global/UndoRedo/UndoRedoStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opyum.Structures.Global
+{
+    public class UndoRedoStack
+    {
+        protected Stack<IUndoRedoMethodCapsule> UndoStack { get; set; } = new Stack<IUndoRedoMethodCapsule>();
+        protected Stack<IUndoRedoMethodCapsule> RedoStack { get; set; } = new Stack<IUndoRedoMethodCapsule>();
+
+        //returns the number of elements in the undo or redo stack
+        public int UndoCount { get => UndoStack.Count; }
+        public int RedoCount { get => RedoStack.Count; }
+
+
+        //event called upon a change in the undo stack
+        public event EventHandler UndoRedoStackChanged;
+
+
+        /// <summary>
+        /// Undo an <see cref="Action{T1}"/> or <see cref="Func{T, TResult}"/>
+        /// </summary>
+        public void Undo()
+        {
+            if (UndoStack.Count > 0)
+            {
+                var last = UndoStack.Pop();
+                last.Invoke();
+                RedoStack.Push(last);
+                UndoRedoStackChanged?.Invoke(this, new EventArgs());
+            }
+
+        }
+
+        /// <summary>
+        /// Use to redo the function done through the <see cref="Do"/> method
+        /// <para>It pulls the function call from the <see cref="RedoStack"/> and calls the method in the function call</para>
+        /// </summary>
+        public void Redo()
+        {
+            if (RedoStack.Count > 0)
+            {
+                var last = RedoStack.Pop();
+                last.Invoke();
+                UndoStack.Push(last);
+                UndoRedoStackChanged?.Invoke(this, new EventArgs());
+            }
+        }
+
+        public void Do<T>(UndoRedoMethodCapsule<T> capsule)
+        {
+            capsule.Invoke();
+            UndoStack.Push(capsule);
+            if (RedoStack.Count > 0)
+            {
+                RedoStack.Clear();
+            }
+            UndoRedoStackChanged?.Invoke(this, new EventArgs());
+        }
+
+        public void Do<T>(Func<object, T, T>func, object target, T val, object caller = null)
+        {
+            Do(new UndoRedoMethodCapsule<T>(func, target, val, caller));
+        }
+
+        public void Do<Tobj, Tval>(Func<Tobj, Tval, Tval> func, object target, Tval val, object caller = null)
+        {
+
+            Do(new UndoRedoMethodCapsule<Tval>((object a, Tval b) => func((Tobj)a, (Tval)b), target, val, caller));
+        }
+
+
+        public void Do<T>(Action<T, T> action, T oldVal, T newVal, object caller = null)
+        {
+            Do(new UndoRedoMethodCapsule<T>(action, oldVal, newVal, caller));
+        }
+
+    }
+}
---
+++ src/Opyum.Structures/Playlist/ContentInfo/ContentInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Opyum.Structures.Playlist
+{
+    /// <summary>
+    /// Audio information.
+    /// <para>The standard things it contains should be simillar to a ID3 tag, incčluding an ID3 tag.</para>
+    /// </summary>
+    public class ContentInfo
+    {
+        /// <summary>
+        /// The title of the song or content
+        /// </summary>
+        public string Title { get; set; }
+
+
+        /// <summary>
+        /// The name of the content, song artist or audio file
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A limited ammount of details for the content, taken from the server
+        /// </summary>
+        public string Description { get; set; }
+
+
+        /// <summary>
+        /// The audio image that will be shown from the content
+        /// </summary>
+        public AudioPicture Image { get; set; }
+
+        /// <summary>
+        /// Temporal length of the content
+        /// <para>If the content is a stream, the length stay default or 0</para>
+        /// </summary>
+        public TimeSpan Length { get; set; } = TimeSpan.Zero;
+
+
+
+        public ContentInfo(string path)
+        {
+            Image = new AudioPicture(path);
+        }
+
+
+
+
+
+
+    }
+}
